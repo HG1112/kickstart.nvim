9d4e65424b4ae16a4c93d03922466a2bd419d17f
Harish Gontu <harishgontu@gmail.com>
2024-02-26 17:01:53 -0800

    rebase

---
+++ lua/kickstart/plugins/explorer.lua
@@ -0,0 +1,20 @@
+-- explorer.lua
+--
+-- Shows how to use the DAP plugin to debug your code.
+--
+-- Primarily focused on configuring the debugger for Go, but can
+-- be extended to other languages as well. That's why it's called
+-- kickstart.nvim and not kitchen-sink.nvim ;)
+
+return {
+  -- NOTE: Yes, you can install new plugins here!
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false,
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("nvim-tree").setup {}
+    end,
+}
